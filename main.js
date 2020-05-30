aca3260dcc704786c77b1c484436df7db36150ae
rocio <rotoledo93@gmail.com>
2020-05-30 11:05:16 -0300

    primercomit

---
+++ main.js
@@ -0,0 +1,11 @@
+  
+  
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("show");
+  });
+
+
+  ScrollReveal().reveal('.principal', { delay: 500 });
+  ScrollReveal().reveal('.baner', { delay: 500 });
+  ScrollReveal().reveal('.noticias', { delay: 500 });
+
